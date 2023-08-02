a1b8ae4dbdffa15f226b09a411e921b011c0d91c
diorizqi404 <rizqiii.dio@gmail.com>
2023-08-03 06:45:01 +0700

    add typed animation

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
+const typed = new Typed('#typed', {
+  strings: ['Student Software Engineering', 'OSINT Analyst','Junior Developer',],
+  typeSpeed: 50,
+  loop: true,
+  delaySpeed : 150,
+});
+
 const button = document.querySelector('#menu-btn');
 const navMenu = document.querySelector('#nav-menu');
 
