7c9752bb6836f5deddf4e0186795401371736f8e
diorizqi404 <rizqiii.dio@gmail.com>
2023-08-17 15:46:01 +0700

    add minor updates

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 const typed = new Typed('#typed', {
-  strings: ['Student Software Engineering', 'OSINT Analyst','Junior Developer',],
+  strings: ['Student Software Engineering', 'Junior Developer', 'Cyber Enthusiast'],
   typeSpeed: 50,
   loop: true,
   delaySpeed : 150,
@@ -11,3 +11,25 @@ const navMenu = document.querySelector('#nav-menu');
 button.addEventListener('click', function() {
     navMenu.classList.toggle('hidden');
 })
+
+// WHEN ON HOME SECTION, REMOVE BUTTON GO TO TOP
+window.onscroll = function() {
+  const header = document.querySelector('#home');
+  const fixNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
+  
+  if(window.pageYOffset > fixNav){
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
+  }else {
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
+  };
+};
+
+// FORMSPREE
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
